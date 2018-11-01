10642ecd743d7afda7fda08bd88694233148cdbe
Tomoya Hayashi <tomoya128@gmail.com>
2018-11-01 23:24:04 +0900

    react redux sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from 'react-router-dom';
+import Ranking from './components/Ranking'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul>
+          <li><Link to="/all">all category</Link></li>
+          <li><Link to="/category/2502">pc etc...</Link></li>
+          <li><Link to="/category/10002">book etc...</Link></li>
+        </ul>
+        <Route path="/all" component={Ranking} />
+        <Route
+          path="/category/:id"
+          render={
+            ({ match }) => <Ranking categoryId={match.params.id}/>
+          }
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import createStore from './createStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+const store = createStore(history);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
